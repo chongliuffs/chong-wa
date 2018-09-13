6a0790b763dbb2eb363a42fce54ac7619f568c7b
chongliuffs <chongliu@fingerfoodstudios.com>
2018-09-13 12:05:36 -0700

    Add forms

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,12 +1,33 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
+using RazorPagesContacts.Data;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
-namespace RazorPagesIntro.Pages {
-	public class IndexModel2 : PageModel {
-		 public string Message { get; private set; } = "PageModel in C#";
+namespace RazorPagesContacts.Pages {
+	public class IndexModel : PageModel {
+		private readonly AppDbContext _db;
 
-		 public void OnGet() {
-			 Message += $" Server time is { DateTime.Now }";
-		 }
+		public IndexModel(AppDbContext db) {
+			_db = db;
+		}
+
+		public IList<Customer> Customers { get; private set; }
+
+		public async Task OnGetAsync() {
+			Customers = await _db.Customers.AsNoTracking().ToListAsync();
+		}
+
+		public async Task<IActionResult> OnPostDeleteAsync(int id) {
+			var contact = await _db.Customers.FindAsync(id);
+
+			if (contact != null) {
+				_db.Customers.Remove(contact);
+				await _db.SaveChangesAsync();
+			}
+
+			return RedirectToPage();
+		}
 	}
 }
--- Startup.cs
+++ Startup.cs
@@ -1,20 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using RazorPagesContacts.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace chong_wa {
 	public class Startup {
+		public IHostingEnvironment HostingEnvironment { get; }
+
 		public void ConfigureServices(IServiceCollection services) {
+			services.AddDbContext<AppDbContext>(options =>
+				options.UseInMemoryDatabase("name"));
 			services.AddMvc();
 		}
 
 		public void Configure(IApplicationBuilder app) {
 			app.UseMvc();
 		}
+	}
+}
+
+
+
+namespace RazorPagesContacts.Data {
+	public class Customer {
+		public int Id { get; set; }
+
+		[Required, StringLength(100)]
+		public string Name { get; set; }
+	}
+
+	public class AppDbContext : DbContext {
+		public AppDbContext(DbContextOptions options) : base(options) {}
+
+		public DbSet<Customer> Customers { get; set; }
 	}
 }
